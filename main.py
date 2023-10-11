9b2189f668e1b4a4d0a6fb64bd950a446de21cda
King San CHU <wkis130@live.rhul.ac.uk>
2023-10-11 15:42:19 +0100

    Print statement for (un)visited coordinates

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+# @author Steven CHU
+
 class player:
     # Record if the player or cell visited the coordinates or not.
     def __init__(cell, x, y):
         cell.x = x
         cell.y = y
         cell.visited = False 
+
+#Check if player visited the coordinates.
+if player.visited:
+    print("Player has visited the corrdinates.")
+else:
+    print("Player has not visited the coordinates.")
