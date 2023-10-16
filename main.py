b25f72d0849e6c332ee057dcc915901807fcb031
King San CHU <wkis130@live.rhul.ac.uk>
2023-10-16 17:42:40 +0100

    Reconstructing and adding maze class

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 
 class player:
     # Record if the player or cell visited the coordinates or not.
-    def __init__(cell, x, y):
-        cell.x = x
-        cell.y = y
-        cell.visited = False 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.visited = False 
 
-#Check if player visited the coordinates.
-if player.visited:
-    print("Player has visited the corrdinates.")
-else:
-    print("Player has not visited the coordinates.")
+    #Check if player visited the coordinates.
+        if player.visited:
+            print("Player has visited the corrdinates.")
+        else:
+            print("Player has not visited the coordinates.")
+
+class Maze:
+    #Representing the maze configuration
+    def __init__(self, maze_data):
+        self.maze_data = maze_data
+        self.start_position = (0,0)
+        self.end_position = (4,4)
