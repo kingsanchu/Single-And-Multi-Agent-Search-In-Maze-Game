86e7530bc8e69797596f72d74ae26a9a1186159f
King San CHU <wkis130@live.rhul.ac.uk>
2023-11-16 01:35:15 +0000

    Implemented dijsktra's algorithm

---
+++ dijsktra.py
@@ -0,0 +1,56 @@
+from mazeGame import maze, agent, textLabel
+from queue import PriorityQueue
+
+def heuristic(cell1, cell2):
+    x1, y1 = cell1
+    x2, y2 = cell2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def dijkstraPathFinding(self):
+    start = (self.rows, self.cols)
+    g_scores = {cell: float('inf') for cell in self.grid}
+    g_scores[start] = 0
+
+    open_cells = PriorityQueue()
+    open_cells.put((0, start))
+    d_path = {}
+
+    while not open_cells.empty():
+        current_cost, current_cell = open_cells.get()
+        if current_cell == (1, 1):
+            break
+        for direction in 'ESNW':
+            if self.maze_map[current_cell][direction] == True:
+                if direction == 'E':
+                    child_cell = (current_cell[0], current_cell[1] + 1)
+                if direction == 'W':
+                    child_cell = (current_cell[0], current_cell[1] - 1)
+                if direction == 'N':
+                    child_cell = (current_cell[0] - 1, current_cell[1])
+                if direction == 'S':
+                    child_cell = (current_cell[0] + 1, current_cell[1])
+
+                temp_g_score = g_scores[current_cell] + 1
+
+                if temp_g_score < g_scores[child_cell]:
+                    g_scores[child_cell] = temp_g_score
+                    open_cells.put((temp_g_score, child_cell))
+                    d_path[child_cell] = current_cell
+
+    forward_path = {}
+    cell = (1, 1)
+    while cell != start:
+        forward_path[d_path[cell]] = cell
+        cell = d_path[cell]
+    return forward_path
+
+if __name__ == '__main__':
+    maze_instance = maze(10, 10)
+    maze_instance.CreateMaze()
+    path = dijkstraPathFinding(maze_instance)
+
+    agent_a = agent(maze_instance, footprints=True)
+    maze_instance.tracePath({agent_a: path})
+    label = textLabel(maze_instance, 'Dijkstra Path Length', len(path) + 1)
+
+    maze_instance.run()
