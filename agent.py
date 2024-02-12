d66690d5d8d5d267bd1604dce974a909f1269b95
King San CHU <wkis130@live.rhul.ac.uk>
2024-02-12 22:23:47 +0000

    Updating position setting

--- agent.py
+++ agent.py
@@ -1,7 +1,6 @@
 from COLOR import COLOR
 
 
-
 class agent:
 
     def __init__(self, parentMaze, x=None, y=None, shape='square', goal=None, filled=False, footprints=False, color: COLOR = COLOR.blue):
@@ -31,115 +30,25 @@ class agent:
         self._body = []
         self.position = (self.x, self.y)
 
-    @property
-    def x(self):
-        return self._x
+    def discard_pickup(self) -> None:
+        ''' This function is called when an enemy was holding a pickup and then discards
+            it, and is used as memory. '''
+        self.pickup_memory = None
 
-    @x.setter
-    def x(self, newX):
-        self._x = newX
+    def determine_path(self, board, start, endpoint_y, endpoint_x) -> deque:
+        ''' Path is towards endpoint destination if the enemy is invulnerable (the normal case).
+            Otherwise, the enemy needs to retreat towards the starting location. '''
 
-    @property
-    def y(self):
-        return self._y
+        if self.invulnerable:
+            return self.breadth_first_search(board, start, endpoint_y, endpoint_x)
 
-    @y.setter
-    def y(self, newY):
-        self._y = newY
-        w = self._parentMaze._cell_width
-        x = self.x*w-w+self._parentMaze._LabWidth
-        y = self.y*w-w+self._parentMaze._LabWidth
-        if self.shape == 'square':
-            if self.filled:
-                self._coord = (y, x, y + w, x + w)
-            else:
-                self._coord = (y + w/2.5, x + w/2.5, y +
-                               w/2.5 + w/4, x + w/2.5 + w/4)
         else:
-            self._coord = (y + w/2, x + 3*w/9, y + w/2, x + 3*w/9+w/4)
+            return self.breadth_first_search(board, start, self.start_location[1], self.start_location[0])[:-1]
 
-        if (hasattr(self, '_head')):
-            if self.footprints is False:
-                self._parentMaze._canvas.delete(self._head)
-            else:
-                if self.shape == 'square':
-                    self._parentMaze._canvas.itemconfig(
-                        self._head, fill=self.color.value[1], outline="")
-                    self._parentMaze._canvas.tag_raise(self._head)
-                    try:
-                        self._parentMaze._canvas.tag_lower(self._head, 'ov')
-                    except:
-                        pass
-                    if self.filled:
-                        cell_coordinates = self._parentMaze._canvas.coords(
-                            self._head)
-                        old_cell = (round(((cell_coordinates[1]-26)/self._parentMaze._cell_width)+1), round(
-                            ((cell_coordinates[0]-26)/self._parentMaze._cell_width)+1))
-                        self._parentMaze._redrawCell(
-                            *old_cell, self._parentMaze.theme)
-                else:
-                    self._parentMaze._canvas.itemconfig(
-                        self._head, fill=self.color.value[1])  # ,outline='gray70')
-                    self._parentMaze._canvas.tag_raise(self._head)
-                    try:
-                        self._parentMaze._canvas.tag_lower(self._head, 'ov')
-                    except:
-                        pass
-                self._body.append(self._head)
-            if not self.filled or self.shape == 'arrow':
-                if self.shape == 'square':
-                    self._head = self._parentMaze._canvas.create_rectangle(
-                        *self._coord, fill=self.color.value[0], outline='')  # stipple='gray75'
-                    try:
-                        self._parentMaze._canvas.tag_lower(self._head, 'ov')
-                    except:
-                        pass
-                else:
-                    self._head = self._parentMaze._canvas.create_line(
-                        *self._coord, fill=self.color.value[0], arrow=FIRST, arrowshape=(3/10*w, 4/10*w, 4/10*w))  # ,outline=self.color.name)
-                    try:
-                        self._parentMaze._canvas.tag_lower(self._head, 'ov')
-                    except:
-                        pass
-                    orientation = self._orient % 4
-                    if orientation == 1:
-                        self.rotate_clockwise()
-                        self._orient -= 1
-                    elif orientation == 3:
-                        self.rotate_counter_clockwise()
-                        self._orient += 1
-                    elif orientation == 2:
-                        self.rotate_counter_clockwise()
-                        self.rotate_counter_clockwise()
-                        self._orient += 2
-            else:
-                self._head = self._parentMaze._canvas.create_rectangle(
-                    *self._coord, fill=self.color.value[0], outline='')  # stipple='gray75'
-                try:
-                    self._parentMaze._canvas.tag_lower(self._head, 'ov')
-                except:
-                    pass
-                self._parentMaze._redrawCell(
-                    self.x, self.y, theme=self._parentMaze.theme)
-        else:
-            self._head = self._parentMaze._canvas.create_rectangle(
-                *self._coord, fill=self.color.value[0], outline='')  # stipple='gray75'
-            try:
-                self._parentMaze._canvas.tag_lower(self._head, 'ov')
-            except:
-                pass
-            self._parentMaze._redrawCell(
-                self.x, self.y, theme=self._parentMaze.theme)
-
-    @property
-    def position(self):
-        return (self.x, self.y)
-
-    @position.setter
-    def position(self, newpos):
-        self.x = newpos[0]
-        self.y = newpos[1]
-        self._position = newpos
+    def determineDirection(self, board, pacman) -> None:
+        ''' Direction is determined by the enemy type. Since each enemy type
+            has their own unique game movement. '''
+        start = self.x, self.y
 
     def rotate_counter_clockwise(self):
         '''
