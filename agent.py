c61fb53014909718a7fd18a720afaf8688fae24b
King San CHU <wkis130@live.rhul.ac.uk>
2024-03-04 06:03:56 +0000

    Implementing multiple agent movements

--- agent.py
+++ agent.py
@@ -1,41 +1,55 @@
-from COLOR import COLOR
+from character import Character
+from wall import Wall
+import pacman
 from collections import deque
 from random import random
-from wall import Wall
 
-class agent:
 
-    def __init__(self, parentMaze, x=None, y=None, shape='square', goal=None, filled=False, footprints=False, color: COLOR = COLOR.blue):
+class agent(Character):
+    inky = 5
+    blinky = 6
+    pinky = 7
+    clyde = 8
 
-        self._parentMaze = parentMaze
-        self.color = color
-        if (isinstance(color, str)):
-            if (color in COLOR.__members__):
-                self.color = COLOR[color]
-            else:
-                raise ValueError(f'{color} is not a valid COLOR!')
-        self.filled = filled
-        self.shape = shape
-        self._orient = 0
-        if x is None:
-            x = parentMaze.rows
-        if y is None:
-            y = parentMaze.cols
-        self.x = x
-        self.y = y
-        self.footprints = footprints
-        self._parentMaze._agents.append(self)
-        if goal == None:
-            self.goal = self._parentMaze._goal
-        else:
-            self.goal = goal
-        self._body = []
-        self.position = (self.x, self.y)
+    def __init__(self, x, y, enemy_type, images, direction=None):
+        ''' Initializes an Enemy class that inherits from the Character Class. The enemy class
+            is one of the two classes that has movement involved, and different attributes to
+            represent the state. The enemy_type is given by argument, and there are 4 different enemy
+            types because each enemy is unique. '''
+        Character.__init__(self, x, y, direction)
+        self.enemy_type = enemy_type
+        self.invulnerable = True
+        self.slowed_down = False
+        self.determine_image(enemy_type, images)
+        self.pickup_memory = None
+
+        if enemy_type == agent.inky or enemy_type == agent.clyde:  # Only Inky and Clyde require these Attributes
+            self.movement_turns = 15
+            self.last_choice = None
 
     def discard_pickup(self) -> None:
         ''' This function is called when an enemy was holding a pickup and then discards
             it, and is used as memory. '''
         self.pickup_memory = None
+
+    def determine_image(self, enemy_type, images) -> None:
+        ''' Image display to player is determined by which type of enemy it is. If the enemy
+            is not invulnerable, then they all have the same common vulnerable ghost image. '''
+        if self.invulnerable:
+            if enemy_type == agent.inky:
+                self._image = images.return_image('inky')
+
+            elif enemy_type == agent.blinky:
+                self._image = images.return_image('blinky')
+
+            elif enemy_type == agent.pinky:
+                self._image = images.return_image('pinky')
+
+            elif enemy_type == agent.clyde:
+                self._image = images.return_image('clyde')
+
+        else:
+            self._image = images.return_image('vulnerable_ghost')
 
     def determine_path(self, board, start, endpoint_y, endpoint_x) -> deque:
         ''' Path is towards endpoint destination if the enemy is invulnerable (the normal case).
@@ -52,25 +66,139 @@ class agent:
             has their own unique game movement. '''
         start = self.x, self.y
 
-    def rotate_counter_clockwise(self):
-        '''
-        To Rotate the agent in Counter Clock Wise direction
-        '''
-        def pointNew(p, newOrigin):
-            return (p[0]-newOrigin[0], p[1]-newOrigin[1])
-        cell_width = self._parentMaze._cell_width
-        x = self.x*cell_width-cell_width+self._parentMaze._LabWidth
-        y = self.y*cell_width-cell_width+self._parentMaze._LabWidth
-        center = (y+cell_width/2, x+cell_width/2)
-        p1 = pointNew((self._coord[0], self._coord[1]), center)
-        p2 = pointNew((self._coord[2], self._coord[3]), center)
-        p1_cw = (p1[1], -p1[0])
-        p2_cw = (p2[1], -p2[0])
-        p1 = p1_cw[0]+center[0], p1_cw[1]+center[1]
-        p2 = p2_cw[0]+center[0], p2_cw[1]+center[1]
-        self._coord = (*p1, *p2)
-        self._parentMaze._canvas.coords(self._head, *self._coord)
-        self._orient = (self._orient-1) % 4
+        if self.enemy_type == agent.blinky:
+            self.blinky_movement(board, start, pacman)
+
+        elif self.enemy_type == agent.inky:
+            self.inky_movement(board, start, pacman)
+
+        if self.enemy_type == agent.pinky:
+            self.pinky_movement(board, start, pacman)
+
+        elif self.enemy_type == agent.clyde:
+            self.clyde_movement(board)
+
+    # Inky Movement Functions #
+    def blinky_movement(self, board, start, pacman) -> None:
+        ''' Blinky's movement is to directly chase Pacman on the board. '''
+        path = self.determine_path(board, start, pacman.y, pacman.x)
+        self.path_finding_direction(path)
+
+    # Blinky Movement Functions #
+    def inky_movement(self, board, start, pacman) -> None:
+        ''' Inky's movement differentiates between the other three ghost. So we use
+            random() from the random library to determine which movement he will follow,
+            and it will constantly be changing as time goes on. '''
+        choice = self.random_choice()
+        self._inky_and_clyde_movement_turns()
+
+        if choice <= .33:
+            self.blinky_movement(board, start, pacman)
+
+        elif choice <= .75:
+            self.clyde_movement(board)
+
+        elif choice <= 1:
+            self.pinky_movement(board, start, pacman)
+
+    # Pinky Movement Functions #
+
+    def pinky_movement(self, board, start, pacman):
+        ''' Pinky's movement is meant to ambush, so we have the entire pacman object
+            so that are we able to look at his direction and coordinates. '''
+        endpoint_y, endpoint_x = self.pinky_endpoints(board, pacman)
+        path = self.determine_path(board, start, endpoint_y, endpoint_x)
+
+        self.path_finding_direction(path)
+
+    def pinky_endpoints(self, board, pacman) -> tuple:
+        ''' This function primarily just returns the endpoints from the method
+            pinky_avoiding_wall. The difference is that it accounts for the direction
+            and adds a change in x or y depending on that direction. '''
+        if pacman.direction == 'Left':
+            return self.pinky_ambush(board, pacman, 0, -1)
+
+        elif pacman.direction == 'Right':
+            return self.pinky_ambush(board, pacman, 0, 1)
+
+        elif pacman.direction == 'Up':
+            return self.pinky_ambush(board, pacman, -1, 0)
+
+        elif pacman.direction == 'Down':
+            return self.pinky_ambush(board, pacman, 1, 0)
+
+    def pinky_ambush(self, board, pacman, dy, dx) -> tuple:
+        ''' This function is used to get ahead of Pacman to ambush him.
+            The max distance to get ahead is set in the local variable
+            ambush_limit. The ambush limit is less if ahead of Pacman
+            is a wall, or the distance is not within board boundaries. '''
+        ambush_limit = 7
+        endpoint_y, endpoint_x = pacman.return_location()
+
+        if self.pacman_within_pinky_proximity(endpoint_y, endpoint_x, ambush_limit):
+            return endpoint_y, endpoint_x
+
+        else:
+            return self.ambush_loop(board, dy, dx, endpoint_y, endpoint_x, ambush_limit)
+
+    def pacman_within_pinky_proximity(self, end_y, end_x, limit):
+        ''' This function checks if an ambush is necessary depending on the distance between
+            Pinky and Pacman. '''
+        return abs(self.y - end_y) < limit and abs(self.x - end_x) < limit
+
+    def ambush_loop(self, board, dy, dx, endpoint_y, endpoint_x, ambush_limit):
+        ''' If the distance between Pinky and Pacman is too great, than this function is called to
+            find a distance within ambush_limit ahead of Pacman so that Pinky can ambush him. '''
+        for i in range(1, ambush_limit):
+            # If the loop can not be completed, then Pinky's endpoints are the slightly adjusted endpoints
+            if self.pinkys_movement_not_within_board(board, endpoint_y + dy, endpoint_x + dx) or \
+                    type(board[endpoint_y + dy][endpoint_x + dx]) == Wall:
+                break
+
+            # Else, the endpoints are getting ahead of Pacman
+            else:
+                endpoint_y += dy
+                endpoint_x += dx
+
+        return endpoint_y, endpoint_x
+
+    def pinkys_movement_not_within_board(self, board, endpoint_dy, endpoint_dx) -> bool:
+        ''' Returns a boolean if the endpoints are not within the board boundaries. '''
+        return not ((0 <= endpoint_dy <= len(board) - 1) and (0 <= endpoint_dx <= board.board_width() - 1))
+
+    # Clyde Movement Functions #
+
+    def clyde_movement(self, board) -> None:
+        ''' Clyde's movement is random, and he does not chase or ambush. That
+            is why it is not required for him to have any endpoint arguments. '''
+        choice = self.random_choice()
+        self._inky_and_clyde_movement_turns()
+        self.random_direction(choice)
+
+        if self.valid_direction(board):
+            self.enemy_moved()
+        else:
+            self.clydes_wrong_direction()
+
+    def clydes_wrong_direction(self) -> None:
+        ''' If clyde has a wrong direction, then his movement_turns are automatically
+            set to 0 so that he can make a random choice on which direction to go.
+            Must specific if enemy is clyde, because Inky can have any three of the
+            other enemy's directions, and this is not the case for him. '''
+        if self.enemy_type == agent.clyde:
+            self.movement_turns = 0
+            self.last_choice = None
+
+        self.last_location = self.return_location()
+
+    def _inky_and_clyde_movement_turns(self) -> None:
+        ''' Inky and clyde are the only enemy with movement_turns attribute, because
+            their movement is based off a random choice that last for 15 updates. '''
+        self._decrement_movement_turns()
+
+        if self.movement_turns == 0:
+            self.movement_turns = 15
+            self.last_choice = None
 
     def _decrement_movement_turns(self) -> None:
         ''' Decrements the attribute movement_turns by 1 until it reaches 0,
